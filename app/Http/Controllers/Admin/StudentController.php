df5dd56b86aa89a548b1a93e157599955b8bad3d
Avicenna <Avicennabintang17>
2025-11-20 11:34:06 +0700

    tabel

---
+++ app/Http/Controllers/Admin/StudentController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('admin.student.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.student.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student)
+    {
+        return view('admin.student.show', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Student $student)
+    {
+        $validated = $request->validate([
+            'nis' => 'required',
+            'nama_lengkap' => 'required',
+            'jenis_kelamin' => 'required',
+            'nisn' => 'required',
+        ]);
+
+        Student->update($validated);
+        return redirect()->route('admin.students.index')->with('success', 'Data siswa berhasil diperbaharui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+{
+    $student = Student::findOrFail($id);
+    $student->delete();
+
+    return redirect()->route('admin.student.index')
+                     ->with('success', 'Data siswa berhasil dihapus');
+}
+
+}
